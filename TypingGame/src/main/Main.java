9e46b13fe47db749bffea4c6ada07d2005afc2e7
cipluk4n <asiilrabbani@gmail.com>
2026-05-25 22:41:13 +0700

    profil-nama

--- TypingGame/src/main/Main.java
+++ TypingGame/src/main/Main.java
@@ -4,6 +4,11 @@ import controller.GameController;
 import view.GameWindow;
 import javax.swing.SwingUtilities;
 
+/*
+    Anggota:
+        123240097 - Alexander
+        1232340099 - Rabbani
+*/
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
